450ce1e8824efb725da553aa38f77eaa753d09e3
Yuankan Fang <yuankanf@hotmail.com>
2018-04-08 23:02:41 -0700

    hw9

---
+++ hw9/set/Wall.java
@@ -0,0 +1,13 @@
+package set;
+
+public class Wall {
+ public    int hOrV;
+ public    int firstCor;
+ public    int secondCor;
+    public Wall(int hOrV, int firstCor, int secondCor){
+        this.hOrV=hOrV;
+        this.firstCor=firstCor;
+        this.secondCor=secondCor;
+    }
+
+}
